34473a82d04d55337ac3098b5385c8db3dae677a
Sitron Te <halsaune@stud.ntnu.no>
2014-03-06 12:32:14 +0100

    Started xml parser implementation
    Started xml parser implementation.
    Added simple test code for parser and an xml notify message.

--- Base/src/org/ntnunotif/wsnu/base/net/XMLParser.java
+++ Base/src/org/ntnunotif/wsnu/base/net/XMLParser.java
@@ -1,8 +1,54 @@
 package org.ntnunotif.wsnu.base.net;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.soap.Node;
+import javax.xml.stream.XMLStreamReader;
+import java.io.InputStream;
+
 /**
  * Created by tormod on 3/3/14.
  */
 public class XMLParser {
 
+    private static String[] classPaths = { "org.oasis_open.docs.wsn.b_2",
+            "org.oasis_open.docs.wsn.br_2",
+            "org.oasis_open.docs.wsn.t_1",
+            "org.oasis_open.docs.wsrf.bf_2",
+            "org.oasis_open.docs.wsrf.r_2"};
+
+    private  static ClassLoader[] factories = {org.oasis_open.docs.wsn.b_2.ObjectFactory.class.getClassLoader(),
+            org.oasis_open.docs.wsn.br_2.ObjectFactory.class.getClassLoader(),
+            org.oasis_open.docs.wsn.t_1.ObjectFactory.class.getClassLoader(),
+            org.oasis_open.docs.wsrf.bf_2.ObjectFactory.class.getClassLoader(),
+            org.oasis_open.docs.wsrf.r_2.ObjectFactory.class.getClassLoader()};
+
+    private XMLParser() {}
+
+    public static void registerReturnObjectPackageAndObjectFactory(String classPath, ClassLoader factoryLoader) {
+        synchronized (XMLParser.class) {
+            // TODO
+        }
+    }
+
+    private static Unmarshaller getUnmarshaller() throws JAXBException {
+        String cp = null;
+        for (String s: classPaths)
+            cp = cp == null ? s : cp + ":" + s;
+        // TODO need all object factories
+       return JAXBContext.newInstance(cp, factories[0]).createUnmarshaller();
+    }
+
+    public static Object parse(Node node) throws JAXBException {
+        return getUnmarshaller().unmarshal(node);
+    }
+
+    public static Object parse(InputStream inputStream) throws JAXBException {
+        return getUnmarshaller().unmarshal(inputStream);
+    }
+
+    public static Object parse(XMLStreamReader xmlStreamReader) throws JAXBException {
+        return getUnmarshaller().unmarshal(xmlStreamReader);
+    }
 }
---
+++ Base/test/ntnunotif/wsnu/base/net/ParserTester.java
@@ -0,0 +1,45 @@
+package ntnunotif.wsnu.base.net;
+
+import org.ntnunotif.wsnu.base.net.XMLParser;
+import org.oasis_open.docs.wsn.b_2.NotificationMessageHolderType;
+import org.oasis_open.docs.wsn.b_2.Notify;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by Inge on 06.03.14.
+ */
+public class ParserTester {
+    public static void main(String[] args) {
+        File file = new File("Base\\test\\ntnunotif\\wsnu\\base\\net\\parse_test_notify.xml");
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            Object parsedObj = XMLParser.parse(fileInputStream);
+            if (parsedObj == null) {
+                System.err.println("Parser returned null value.");
+            } else if (parsedObj.getClass() != Notify.class) {
+                System.err.println("Parser returned wrong object type;");
+                System.err.println("\t" + Notify.class.toString() + " expected");
+                System.err.println("\t" + parsedObj.getClass().toString() + " found");
+            } else {
+                Notify message = (Notify)parsedObj;
+                System.out.println("Printing content of notify message");
+                System.out.println("\tAny:");
+                for (Object obj : message.getAny())
+                    System.out.println("\t\t" + obj);
+                System.out.println("\tNotificationMessage:");
+                for (NotificationMessageHolderType notMess: message.getNotificationMessage())
+                    System.out.println("\t\t"+notMess.toString());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not locate XML file.");
+            e.printStackTrace();
+        } catch (JAXBException e) {
+            System.err.println("Could not parse XML file");
+            e.printStackTrace();
+        }
+    }
+}
