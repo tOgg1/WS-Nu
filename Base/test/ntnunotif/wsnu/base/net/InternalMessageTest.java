77f1fa1e4da8615b2f47cb78a767bb37cef9f884
Tormod Haugland <tormod.haugland@gmail.com>
2014-03-15 11:54:38 +0100

    Wrapped up InternalMessage test

--- Base/test/ntnunotif/wsnu/base/net/InternalMessageTest.java
+++ Base/test/ntnunotif/wsnu/base/net/InternalMessageTest.java
@@ -20,17 +20,24 @@ public class InternalMessageTest extends TestCase {
 
         assertTrue((STATUS_OK & messageOk.statusCode) > 0);
         assertTrue((STATUS_FAULT & messageFault.statusCode) > 0);
-        assertTrue((STATUS_INVALID_DESTINATION & messageOk.statusCode) > 0);
+        assertTrue((STATUS_INVALID_DESTINATION & messageWrongEndpoint.statusCode) > 0);
 
-        assertTrue((STATUS_OK & messageOk.statusCode) == 0);
+        assertTrue((STATUS_FAULT & messageOk.statusCode) == 0);
 
         /* Test various fault-flags */
-        InternalMessage messageInternalFault = new InternalMessage(STATUS_FAULT_INTERNAL_ERROR, null);
-        InternalMessage messageUnknownFault = new InternalMessage(STATUS_FAULT_UNKNOWN_METHOD, null);
+        InternalMessage messageInternalFault = new InternalMessage(STATUS_FAULT|STATUS_FAULT_INTERNAL_ERROR, null);
+        InternalMessage messageUnknownFault = new InternalMessage(STATUS_FAULT|STATUS_FAULT_UNKNOWN_METHOD, null);
 
         assertTrue((STATUS_FAULT & messageInternalFault.statusCode) > 0);
         assertTrue((STATUS_FAULT_INVALID_PAYLOAD & messageInternalFault.statusCode) == 0);
+        assertTrue((STATUS_FAULT_UNKNOWN_METHOD & messageUnknownFault.statusCode) > 0);
 
         /* Test multiple codes */
+        InternalMessage messageInternalFaultUnknownDestination = new InternalMessage(STATUS_FAULT|STATUS_FAULT_INTERNAL_ERROR|STATUS_INVALID_DESTINATION, null);
+
+        assertTrue((STATUS_FAULT & messageInternalFaultUnknownDestination.statusCode) > 0);
+        assertTrue((STATUS_FAULT_INTERNAL_ERROR & messageInternalFaultUnknownDestination.statusCode) > 0);
+        assertTrue((STATUS_INVALID_DESTINATION & messageInternalFaultUnknownDestination.statusCode) > 0);
+        assertTrue(((STATUS_FAULT | STATUS_FAULT_INTERNAL_ERROR) & messageInternalFaultUnknownDestination.statusCode) > 0);
     }
 }
