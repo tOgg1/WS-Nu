cdde3e3c2f3e22ba2595a81442a2afbc3d790d5c
Sitron Te <halsaune@stud.ntnu.no>
2014-04-30 14:52:02 +0200

    Javadoc for NamespaceContextResolver added

--- Base/src/main/java/org/ntnunotif/wsnu/base/net/NuNamespaceContextResolver.java
+++ Base/src/main/java/org/ntnunotif/wsnu/base/net/NuNamespaceContextResolver.java
@@ -11,6 +11,12 @@ public class NuNamespaceContextResolver {
     private Map<Object, ScopeLevelBindings> scopeLevelBindingsMap = new HashMap<>();
     private ScopeLevelBindings currentScope = null;
 
+    /**
+     * Gets the namespace connected to the object given.
+     *
+     * @param object the object to resolve context for
+     * @return a valid {@link javax.xml.namespace.NamespaceContext} if the object is known to this resolver. <code>null</code> otherwise.
+     */
     public NamespaceContext resolveNamespaceContext(Object object) {
         if (scopeLevelBindingsMap.containsKey(object)) {
             return new NuResolvedNamespaceContext(scopeLevelBindingsMap.get(object));
@@ -18,6 +24,12 @@ public class NuNamespaceContextResolver {
         return null;
     }
 
+    /**
+     * Registers an object to the current scope of the resolver.
+     *
+     * @param object the object to bind to a {@link javax.xml.namespace.NamespaceContext}
+     * @throws java.lang.IllegalStateException if no scopes are open
+     */
     public void registerObjectWithCurrentNamespaceScope(Object object) {
         if (currentScope == null) {
             throw new IllegalStateException("No scopes open, can not register binding!");
@@ -25,12 +37,20 @@ public class NuNamespaceContextResolver {
         scopeLevelBindingsMap.put(object, currentScope);
     }
 
+    /**
+     * Opens a new scope for {@link javax.xml.namespace.NamespaceContext}s. Must be called before any objects are registered to the resolver, and before any namespace bindings are registered
+     */
     public void openScope() {
         ScopeLevelBindings newScope = new ScopeLevelBindings();
         newScope.parent = currentScope;
         currentScope = newScope;
     }
 
+    /**
+     * Closes the current scope for {@link javax.xml.namespace.NamespaceContext} resolving.
+     *
+     * @throws java.lang.IllegalStateException if no scopes are open
+     */
     public void closeScope() {
         if (currentScope != null) {
             currentScope = currentScope.parent;
@@ -39,6 +59,13 @@ public class NuNamespaceContextResolver {
         }
     }
 
+    /**
+     * Binds a prefix to a namespace in the current scope.
+     *
+     * @param prefix    the prefix to bind
+     * @param namespace the namespace the prefix should bind to
+     * @throws java.lang.IllegalStateException if no scopes are open
+     */
     public void putNamespaceBinding(String prefix, String namespace) {
 
         if (currentScope == null) {
@@ -52,11 +79,17 @@ public class NuNamespaceContextResolver {
         currentScope.bindings.put(prefix, namespace);
     }
 
+    /**
+     * Private class to keep track of scopes.
+     */
     private static class ScopeLevelBindings {
         ScopeLevelBindings parent;
         Map<String, String> bindings;
     }
 
+    /**
+     * Private class representing the actual namespaces that are returned.
+     */
     private static class NuResolvedNamespaceContext implements NamespaceContext {
 
         final ScopeLevelBindings scopeSource;
