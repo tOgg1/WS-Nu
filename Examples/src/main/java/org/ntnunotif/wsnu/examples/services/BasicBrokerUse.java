e1780fb9e725d90c14647e1673cbc2f385e066d0
Tormod Haugland <tormod.haugland@gmail.com>
2014-05-10 11:29:47 +0200

    Added some very minimalistic examples

---
+++ Examples/src/main/java/org/ntnunotif/wsnu/examples/services/BasicBrokerUse.java
@@ -0,0 +1,38 @@
+package org.ntnunotif.wsnu.examples.services;
+
+import org.ntnunotif.wsnu.base.internal.SoapForwardingHub;
+import org.ntnunotif.wsnu.services.implementations.notificationbroker.GenericNotificationBroker;
+
+/**
+ * Example showing very basic use of the NotificationBroker. This example will not go detailed into how
+ * filters etc. are created. This information can be found in {@link org.ntnunotif.wsnu.examples.services.BasicProducerUse}.
+ */
+public class BasicBrokerUse {
+
+    private GenericNotificationBroker broker;
+
+    private final String brokerEndpoint = "myBroker";
+
+    private SoapForwardingHub myHub;
+    /**
+     * We can do everything needed in the constructor
+     */
+    public BasicBrokerUse() {
+        // Instantiate the broker
+        broker = new GenericNotificationBroker();
+
+        // By calling the brokers quickbuild method, we are starting both the broker and the rest of the
+        // system, all in one.
+        myHub = broker.quickBuild(brokerEndpoint);
+
+        // This method makes our Broker not demand that a publisher publishing on the broker
+        // is registered with the broker beforehand
+        broker.setDemandRegistered(false);
+
+        // This method makes our broker cache messages, for use with the getCurrentMessage web method.
+        broker.setCacheMessages(true);
+
+        // This is the most minimalistic example of creating a NotificationBroker possible. For more information
+        // on filtersupport etc. see the BasicProducerUse example
+    }
+}
---
+++ Examples/src/main/java/org/ntnunotif/wsnu/examples/services/ProducerWithSubscriptionManager.java
@@ -0,0 +1,43 @@
+package org.ntnunotif.wsnu.examples.services;
+
+import org.ntnunotif.wsnu.base.internal.SoapForwardingHub;
+import org.ntnunotif.wsnu.services.implementations.notificationproducer.GenericNotificationProducer;
+import org.ntnunotif.wsnu.services.implementations.subscriptionmanager.SimplePausableSubscriptionManager;
+
+/**
+ * Created by tormod on 10.05.14.
+ */
+public class ProducerWithSubscriptionManager {
+
+    private GenericNotificationProducer producer;
+    private SimplePausableSubscriptionManager manager;
+
+    private SoapForwardingHub myHub;
+
+    private final String producerEndpoint = "myProducer";
+    private final String managerEndpoint = "myManager";
+
+    /**
+     * Everything that needs to be done to set the system up can be done in the constructor.
+     */
+    public ProducerWithSubscriptionManager() {
+        // Instantiate the Web Services
+        producer = new GenericNotificationProducer();
+
+        // We are using a pausable subscription manager here. A regular one could be added in the same fashion
+        manager = new SimplePausableSubscriptionManager();
+
+        // Quick build the Web services, thus initializing the entire system
+        myHub = producer.quickBuild(producerEndpoint);
+        manager.quickBuild(managerEndpoint, myHub);
+
+        // This is the crucial part, we have to add the manager to the producer. If not, the producer
+        // will not be able to receive events when a subscription is changed at the manager
+        producer.setSubscriptionManager(manager);
+
+        // And we are done really. Obviously you can at this stage start creating filters and similar voodoo. But this will
+        // not be covered here.
+    }
+
+
+}
